1b0bc7aac3b4a3f75776aed9fb8df1539950d1fc
Sushiy <julivizi@yahoo.de>
2015-07-11 19:39:34 +0200

    switched to manual movement

--- game/Assets/Scripts/AI_Project/Movement/AI_Movement.cs
+++ game/Assets/Scripts/AI_Project/Movement/AI_Movement.cs
@@ -7,6 +7,7 @@ public class AI_Movement : Movement
 
 	public Vector3 subtarget;
 
+
 	
 	private NavMeshAgent agent;
 
@@ -15,25 +16,33 @@ public class AI_Movement : Movement
 	private Vector3 attraction;
 	private Vector3 repulsion;
 	private Vector3 direction;
+	private int subtargetIndex = 1;
+
+	private NavMeshPath path;
 
 	public override void Awake()
 	{
 		base.Awake();
+		path = new NavMeshPath();
 		agent = GetComponent<NavMeshAgent>();
 		InvokeRepeating("AIUpdate",0.5f,0.5f);
 	}
 
 	void AIUpdate()
 	{	
-		agent.SetDestination(target.position);
-		NavMeshPath path = new NavMeshPath();
+		//agent.SetDestination(target.position);
+		path.ClearCorners();
+		agent.enabled = true;
 		agent.CalculatePath(target.position, path);
-		
+		agent.enabled = false;
+
 		Debug.DrawLine(transform.position, path.corners[0],Color.red,0.5f);
 		for(int i = 1; i < path.corners.Length; i++)
 		{
 			Debug.DrawLine(path.corners[i-1], path.corners[i],Color.red,0.5f);
 		}
+		subtargetIndex = 1;
+		subtarget = path.corners[subtargetIndex];
 	}
 
 	void FixedUpdate()
@@ -41,19 +50,25 @@ public class AI_Movement : Movement
 
 		if(target != null)
 		{
-			direction = agent.velocity.normalized;
+			//direction = agent.velocity.normalized;
+			if((subtarget - transform.position).sqrMagnitude < 2)
+			{
+				subtargetIndex++;
+				subtarget = path.corners[subtargetIndex];
+			}
+			Seek();
 		}
 		float h = direction.x;
 		float v = direction.z;
 
 		Debug.DrawRay(transform.position + new Vector3(0.0f, 0.5f, 0.0f), direction * 3,Color.red);
 
-		agent.Resume();
+		//agent.Resume();
 
 		if (playerAttacks.getIsStronging() || blockMovement
 		    || getKnockdown())
 		{
-			agent.Stop();
+			//agent.Stop();
 			//Debug.Log("stopped");
 			h = 0;
 			v = 0;
@@ -62,13 +77,16 @@ public class AI_Movement : Movement
 		bool walking = h != 0f || v != 0f;
 		if(onFloor)
 		{
-			if(!agent.enabled)
+			/*if(!agent.enabled)
 				agent.enabled = true;
+			*/
+			Move (h, v);
+			Turning(h, v, walking);
 			Animating (h, v, walking);
 		}
 		else
 		{
-			agent.enabled = false;
+			/*agent.enabled = false;*/
 			Fall();
 		}
 
@@ -77,7 +95,7 @@ public class AI_Movement : Movement
 	Vector3 Seek()
 	{
 
-		direction = target.position - transform.position;
+		direction = subtarget - transform.position;
 		direction = new Vector3(direction.x, 0.0f, direction.z);
 
 		sqrTargetDist = direction.sqrMagnitude;
