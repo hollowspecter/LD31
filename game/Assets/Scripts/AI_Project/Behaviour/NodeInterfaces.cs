7d5037aa6dcc585fd3da42ca001ec2976d35d05b
Sushiy <julivizi@yahoo.de>
2015-07-13 13:36:56 +0200

    Behaviour groundwork
    fixed pathfinding stutter
    cleaned and commented AI_Movement
    Added Folderstructure and interface

---
+++ game/Assets/Scripts/AI_Project/Behaviour/NodeInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ChildNode
+{
+	void activate();
+}
+
+public interface ParentNode
+{
+	void AddChild(ChildNode c);
+	bool childDone(ChildNode c);
+}
+
+public interface TaskNode
+{
+	void performTask();
+}
--- game/Assets/Scripts/AI_Project/Movement/AI_Movement.cs
+++ game/Assets/Scripts/AI_Project/Movement/AI_Movement.cs
@@ -13,9 +13,12 @@ public class AI_Movement : Movement
 
 	private float sqrTargetDist;
 	private float stopDist = 9.0f;
+
 	private Vector3 attraction;
 	private Vector3 repulsion;
-	private Vector3 direction;
+
+	private Vector3 subDirection;
+	private float sqrSubDist;
 	private int subtargetIndex = 1;
 
 	private NavMeshPath path;
@@ -28,19 +31,25 @@ public class AI_Movement : Movement
 		InvokeRepeating("AIUpdate",0.5f,0.5f);
 	}
 
+	//this is the Update Method in which the AI gets to think
+		//it is called every half-second
 	void AIUpdate()
 	{	
-		//agent.SetDestination(target.position);
-		path.ClearCorners();
+		//Use Unitys NavMeshAgent to calculate a Path on the NavMesh
+		path.ClearCorners();//Clear the Old Path(necessary?)
 		agent.enabled = true;
 		agent.CalculatePath(target.position, path);
 		agent.enabled = false;
 
+		//Debug.draw the path in red
 		Debug.DrawLine(transform.position, path.corners[0],Color.red,0.5f);
 		for(int i = 1; i < path.corners.Length; i++)
 		{
 			Debug.DrawLine(path.corners[i-1], path.corners[i],Color.red,0.5f);
 		}
+
+		//the subtarget is the point on the Path the AI wants to move towards
+		//it is resetted to the first pathcorner when a new path is calculated
 		subtargetIndex = 1;
 		subtarget = path.corners[subtargetIndex];
 	}
@@ -50,58 +59,72 @@ public class AI_Movement : Movement
 
 		if(target != null)
 		{
-			//direction = agent.velocity.normalized;
-			if((subtarget - transform.position).sqrMagnitude < 2)
+
+			//if you have almost reached your subgoal, move on to the next one to avoid stuttering
+			if(sqrSubDist < 3)
 			{
 				subtargetIndex++;
 				subtarget = path.corners[subtargetIndex];
 			}
-			Seek();
+
+			if(sqrTargetDist < stopDist)
+			{
+				Debug.Log("Arrived");
+			}
+
+			//calculate the direction the character should move in 
+			SeekSubtarget(); //seek your target
+			//TODO: walk around obstacles
+			//TODO: evade dangerous positions (Does this belong here?)
 		}
-		float h = direction.x;
-		float v = direction.z;
 
-		Debug.DrawRay(transform.position + new Vector3(0.0f, 0.5f, 0.0f), direction * 3,Color.red);
+		//split the direction Vector in a horizontal and vertical component
+		//this is mainly a relict of the PlayerMovement  axis-implementation
+		float h = subDirection.x;
+		float v = subDirection.z;
 
-		//agent.Resume();
+		//Debug.draw the direction you want to move in
+		Debug.DrawRay(transform.position + new Vector3(0.0f, 0.5f, 0.0f), subDirection.normalized * 3,Color.red);
 
+
+		//reset the movement-values if the character is Attacking(strong), his movement is blocked or he is knocked Down
 		if (playerAttacks.getIsStronging() || blockMovement
 		    || getKnockdown())
 		{
-			//agent.Stop();
 			//Debug.Log("stopped");
 			h = 0;
 			v = 0;
 		}
 
+		//if either one of the movement values is not zero the character should be walking
 		bool walking = h != 0f || v != 0f;
+
+		//Move, Turn and Animate the character if he is on solid ground
 		if(onFloor)
 		{
-			/*if(!agent.enabled)
-				agent.enabled = true;
-			*/
 			Move (h, v);
 			Turning(h, v, walking);
 			Animating (h, v, walking);
 		}
+		//otherwise let the character Fall
 		else
 		{
-			/*agent.enabled = false;*/
 			Fall();
 		}
 
 	}
 
-	Vector3 Seek()
+	//Calculate the direction to the subtarget and calculate the squared Distance
+	Vector3 SeekSubtarget()
 	{
 
-		direction = subtarget - transform.position;
-		direction = new Vector3(direction.x, 0.0f, direction.z);
+		subDirection = subtarget - transform.position;
+		subDirection = new Vector3(subDirection.x, 0.0f, subDirection.z);
 
-		sqrTargetDist = direction.sqrMagnitude;
+		sqrSubDist = subDirection.sqrMagnitude;
 
 
-		return Vector3.Normalize(direction);
+		return Vector3.Normalize(subDirection);
 
 	}
 
